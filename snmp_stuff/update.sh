d504b4d26e934d89ae985d773194eaaa034a41ff
Scott Baker <smbaker@gmail.com>
2019-11-23 23:14:18 -0800

    synology dashboard

---
+++ snmp_stuff/update.sh
@@ -0,0 +1,4 @@
+export MIBDIRS=/home/smbaker/gowork/src/github.com/prometheus/snmp_exporter/generator/mibs
+#cp ~/projects/pi/smbaker-infrastructure/snmp_stuff/generator.yml generator_workdir/
+#cd generator_workdir
+./generator generate
