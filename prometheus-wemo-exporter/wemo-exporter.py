920a10060c305108740bb0c24af44f8a6d32fd10
David Putzolu <dputzolu@gmail.com>
2020-10-01 14:42:21 -0700

    Fix power metric, remove unneeded metrics

--- prometheus-wemo-exporter/wemo-exporter.py
+++ prometheus-wemo-exporter/wemo-exporter.py
@@ -11,16 +11,10 @@ import sys
 from time import sleep
 from envargparse import EnvArgParser, EnvArgDefaultsHelpFormatter
 
-class wemoInfluxdbExporter:
+class wemoPrometheusExporter:
   def __init__(self):
     try:
       self._args = {}
-      # self._interval = 15
-      # self._wemo_ip = ""
-      # self._wemo_port = 0
-      # self._influxdb_ip = ""
-      # self._influxdb_port = 8086
-      # self._influxdb_database = ""
       self.main()
     except Exception as e:
       print('Error - {}'.format(e))
@@ -28,7 +22,7 @@ class wemoInfluxdbExporter:
 
   def process_args(self):
     parser = EnvArgParser\
-          ( prog="Wemo Influxdb Exporter"
+          ( prog="Wemo Prometheus Exporter"
           , formatter_class=EnvArgDefaultsHelpFormatter
           )
     parser.add_argument\
@@ -38,6 +32,7 @@ class wemoInfluxdbExporter:
         , type=int
         , nargs="?"
         , default=15
+        , const=True
         , help="How often data should be pulled from the Wemo and exported to influxdb"
         )
     parser.add_argument\
@@ -46,6 +41,7 @@ class wemoInfluxdbExporter:
         , env_var="WEMO_IP"
         , nargs="?"
         , default="localhost"
+        , const=True
         , help="IP address for the Wemo to pull data from"
         )
     parser.add_argument\
@@ -54,6 +50,7 @@ class wemoInfluxdbExporter:
         , env_var="WEMO_DEVICE"
         , nargs="?"
         , default="Wemo Insight"
+        , const=True
         , help="name of the Wemo device being queried"
         )
     parser.add_argument\
@@ -61,41 +58,48 @@ class wemoInfluxdbExporter:
         , required=True
         , env_var="EXPORTER_PORT"
         , type=int
+        , const=True
         , nargs="?"
         , help="Port number the exporter should bind to"
+        )
+    parser.add_argument\
+        ( '--verbose'
+        , required=False
+        , env_var="EXPORTER_VERBORSE"
+        , action='store_true'
+        , default=False
+        , help="Enable verbose debug outputs"
         )
 
     self._args = parser.parse_args()
     print(self._args)
 
+  def update_field(self, prometheus_field, wemo_field_value):
+    prometheus_field.labels(self._args.wemo_device) \
+      .set(wemo_field_value)
+    if self._args.verbose:
+      print("%s is %s" % (prometheus_field, wemo_field_value))
+
   def collect(self):
     device = pywemo.discovery.device_from_description( \
       self._wemo_url, None)
-    self._power.labels(self._args.wemo_device) \
-      .set(device.current_power)
-    self._today_kwh.labels(self._args.wemo_device) \
-      .set(device.today_kwh)
-    self._today_on_time.labels(self._args.wemo_device) \
-      .set(device.today_on_time)
-    self._today_standby_time.labels(self._args.wemo_device) \
-      .set(device.today_standby_time)
+    self.update_field(self._power, device.current_power / 1000)
 
   def main(self):
     self.process_args()
-    self._wemo_port = pywemo.ouimeaux_device.probe_wemo(self._args.wemo_ip)
+    port = pywemo.ouimeaux_device.probe_wemo(self._args.wemo_ip)
     self._wemo_url = 'http://%s:%i/setup.xml' \
-      % (self._args.wemo_ip, self._wemo_port)
+      % (self._args.wemo_ip, port)
+    if self._args.verbose:
+      print("wemo url is %s" % self._wemo_url)
+      print("Device is %s" % \
+        pywemo.discovery.device_from_description(self._wemo_url, None))
     self._power = \
       Gauge('power', 'Instantaneous power consumption', ['device'])
-    self._today_kwh =\
-       Gauge('today_kwh', 'kWh consumed since midnight', ['device'])
-    self._today_on_time = \
-      Gauge('today_on_time', 'On time since midnight', ['device'])
-    self._today_standby_time = \
-      Gauge('today_standby_time', 'Off time since midnight', ['device'])
     start_http_server(self._args.port)
+    
     while True:
       self.collect()
       sleep(self._args.interval)
 
-wemoInfluxdbExporter()
+wemoPrometheusExporter()
